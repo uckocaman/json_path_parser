7294ae8394e890f141c750cd1505f1609d5085e5
Umut Can KOCAMAN <33186542+uckocaman@users.noreply.github.com>
2022-11-05 17:57:35 +0300

    fixed error

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def json_field_path(json_data):
         elif isinstance(val, list):
             if len(val) == 0:
                 fields_list.append(k)
-                data_types.append('Empty Array')
+                data_types.append("Empty Array")
             elif isinstance(val[0], dict):
                 fields_list += [k + "." + x for x in json_field_path(val[0])]
             else:
@@ -41,12 +41,14 @@ def json_field_path(json_data):
 
 def write_to_excel():
     df = pd.DataFrame(list(zip(fields, data_types)), columns=["Field", "Data Type"])
-    df.loc[df['Data Type'] == 'str', 'Data Type'] = 'string'
-    df.loc[df['Data Type'] == 'int', 'Data Type'] = 'integer'
-    df.loc[df['Data Type'] == 'bool', 'Data Type'] = 'boolean'
+    df.loc[df["Data Type"] == "str", "Data Type"] = "string"
+    df.loc[df["Data Type"] == "int", "Data Type"] = "integer"
+    df.loc[df["Data Type"] == "bool", "Data Type"] = "boolean"
 
-    dates = ['Date', 'date', 'Time', 'time']
-    df.loc[df['Field'].str.contains('|'.join(dates)), 'Data Type'] = 'timestamp (please check from document)'
+    dates = ["Date", "date", "Time", "time"]
+    df.loc[
+        df["Field"].str.contains("|".join(dates)), "Data Type"
+    ] = "timestamp (please check from document)"
 
     df["Description"] = ""
     df.to_excel("output.xlsx", index=False)
