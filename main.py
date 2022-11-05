98769bc8aaaf3ed7f520d9d36235f0cf38095ada
Umut Can KOCAMAN <33186542+uckocaman@users.noreply.github.com>
2022-11-05 17:50:54 +0300

    data type assignment changed

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from dateutil.parser import parse
 import json
 import sys
 
+
 def json_file(file_path):
     f = open(file_path)
     json_data = json.load(f)
-    
+
     return json_data
+
 
 def json_field_path(json_data):
     if not isinstance(json_data, dict):
         return json_data
-    
+
     fields_list = []
     global data_types
 
@@ -21,38 +23,37 @@ def json_field_path(json_data):
         if isinstance(val, dict):
             fields_list += [k + "." + x for x in json_field_path(val)]
         elif isinstance(val, list):
-            fields_list.append(k)
-            if type(val[0]).__name__ == 'str':
-                try: 
-                    parse(val[0])
-                    data_types.append('timestamp')
-                except ValueError:
-                    data_types.append(type(val[0]).__name__)
+            if len(val) == 0:
+                fields_list.append(k)
+                data_types.append('Empty Array')
+            elif isinstance(val[0], dict):
+                fields_list += [k + "." + x for x in json_field_path(val[0])]
             else:
+                fields_list.append(k)
                 data_types.append(type(val[0]).__name__)
         else:
             fields_list.append(k)
-            if type(val).__name__ == 'str':
-                try: 
-                    parse(val[0])
-                    data_types.append('timestamp')
-                except ValueError:
-                    data_types.append(type(val[0]).__name__)
-            else:
-                data_types.append(type(val).__name__)
+            data_types.append(type(val).__name__)
     return fields_list
 
-def write_to_excel(fields,data_types):
-    df = pd.DataFrame(list(zip(fields, data_types)),columns =['Field', 'Data Type'])
-    df['Data Type'] = np.where(df['Data Type'] == "str", "string",
-                       np.where(df['Data Type'] == "int", "integer",
-                       np.where(df['Data Type'] == "bool", "boolean", df['Data Type'])))
-    df['Description'] = ''
 
-    df.to_excel('output.xlsx', index=False)
 
-if __name__ == '__main__':
+
+def write_to_excel():
+    df = pd.DataFrame(list(zip(fields, data_types)), columns=["Field", "Data Type"])
+    df.loc[df['Data Type'] == 'str', 'Data Type'] = 'string'
+    df.loc[df['Data Type'] == 'int', 'Data Type'] = 'integer'
+    df.loc[df['Data Type'] == 'bool', 'Data Type'] = 'boolean'
+
+    dates = ['Date', 'date', 'Time', 'time']
+    df.loc[df['Field'].str.contains('|'.join(dates)), 'Data Type'] = 'timestamp (please check from document)'
+
+    df["Description"] = ""
+    df.to_excel("output.xlsx", index=False)
+
+
+if __name__ == "__main__":
     data_types = []
     json_data = json_file(sys.argv[1])
     fields = json_field_path(json_data)
-    write_to_excel(fields,data_types)
+    write_to_excel(fields, data_types)
